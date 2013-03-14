f395c8c6209e8e1ebf3ba4b7677e4041d27d9aed
fmj <fmj200x@gmail.com>
2013-03-14 11:57:19 +0100

    adhere to the rules otherwise the goblings will annoy us a little

--- GildedRose.Classes/Items.cs
+++ GildedRose.Classes/Items.cs
@@ -2,18 +2,10 @@
 {
     public class Item
     {
-        private int _quality = 0;
-
-        public Item()
-        {
-
-        }
         public string Name { get; set; }
 
         public int SellIn { get; set; }
 
         public int Quality { get; set; }
-
-
     }
 }
